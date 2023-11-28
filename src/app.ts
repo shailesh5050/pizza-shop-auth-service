dc9c0d636a2138820d9919d986a641416b46cc4c
shailesh5050 <shaileshkuswaha1@gmail.com>
2023-11-28 13:11:39 +0530

    code formated

--- src/app.ts
+++ src/app.ts
@@ -4,31 +4,27 @@ const app = express();
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.get('/', async (_req: Request, res: Response) => {
-   
     res.send('Hello world!');
-}
-);
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: unknown, req:Request, res: Response, next: NextFunction) => {
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         logger.error(err.message);
-        const statusCode =  500;
+        const statusCode = 500;
         res.status(statusCode).json({
             errors: [
                 {
-                    type : err.name,
+                    type: err.name,
                     msg: err.message,
                     path: '',
-                    location: ''
-
-                }
-            ]
+                    location: '',
+                },
+            ],
         });
-
     } else {
         res.status(400).send('Bad request');
     }
-})
+});
 
 export default app;
